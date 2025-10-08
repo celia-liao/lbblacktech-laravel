4be2299679bd8c8bfa0cdc9883a11e3c5eeb8982
celia-liao <infinite010750@gmail.com>
2025-10-08 09:57:12 +0800

    將取色器從slider替換成sketch

--- app/Nova/WebsiteStyle.php
+++ app/Nova/WebsiteStyle.php
@@ -54,46 +54,46 @@ class WebsiteStyle extends Resource
             ID::make('Style ID', 'style_id')->sortable(),
             BelongsTo::make('Pet', 'pet', \App\Nova\Pet::class)
                 ->display('pet_name'), 
-            Color::make('Loading Color', 'loading_color')->slider()
+            Color::make('Loading Color', 'loading_color')->sketch()
                 ->sortable()
                 ->help('loading背景顏色'),
-            Color::make('Cover Name Color', 'cover_name_color')->slider()
+            Color::make('Cover Name Color', 'cover_name_color')->sketch()
                 ->sortable()
                 ->help('封面名稱的圈圈 / 一起度過的日子(數字) / 生命軌跡時間軸顏色'),
             Color::make('Header Love Color', 'header_love_color')->sketch()
                 ->sortable()
                 ->help('封面愛心顏色'),
-            Color::make('Header Footprint Color', 'header_footprint_color')->slider()
+            Color::make('Header Footprint Color', 'header_footprint_color')->sketch()
                 ->sortable()
                 ->help('封面名稱的圈圈內腳印顏色'),
-            Color::make('Day Text Color', 'day_text_color')->slider()
+            Color::make('Day Text Color', 'day_text_color')->sketch()
                 ->sortable()
                 ->help('生命軌跡文字 / 年紀顏色'),
-            Color::make('Title Color', 'title_color')->slider()
+            Color::make('Title Color', 'title_color')->sketch()
                 ->sortable()
                 ->help('logo標題 / 標題 / footer文字顏色'),
-            Color::make('Handshake Button Color', 'handshake_button_color')->slider()
+            Color::make('Handshake Button Color', 'handshake_button_color')->sketch()
                 ->sortable()
                 ->help('握手互動按鈕顏色'),
-            Color::make('Videos Button Color', 'videos_button_color')->slider()
+            Color::make('Videos Button Color', 'videos_button_color')->sketch()
                 ->sortable()
                 ->help('其他互動按鈕顏色'),
-            Color::make('Bubble Ball Color', 'bubble_ball_color')->slider()
+            Color::make('Bubble Ball Color', 'bubble_ball_color')->sketch()
                 ->sortable()
                 ->help('泡泡顏色'),
             Text::make('Bubble Background', 'bubble_background')
                 ->sortable()
                 ->help('泡泡背景漸層 ex:linear-gradient(180deg, rgba(255, 241, 229, 0) 0%, rgb(255, 217, 179) 81.6%)'),
-            Color::make('Footprint Color', 'footprint_color')->slider()
+            Color::make('Footprint Color', 'footprint_color')->sketch()
                 ->sortable()
                 ->help('腳印顏色'),
-            Color::make('Footer Background Color', 'footer_background_color')->slider()
+            Color::make('Footer Background Color', 'footer_background_color')->sketch()
                 ->sortable()
                 ->help('頁尾背景顏色'),
-            Color::make('Function Color', 'function_color')->slider()
+            Color::make('Function Color', 'function_color')->sketch()
                 ->sortable()
                 ->help('狀態列顏色'),
-            Color::make('Function Background Color', 'function_background_color')->slider()
+            Color::make('Function Background Color', 'function_background_color')->sketch()
                 ->sortable()
                 ->help('狀態列背景顏色'),
         ];
