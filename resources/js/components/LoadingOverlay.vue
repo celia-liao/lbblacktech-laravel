e59f5e23ddd961ec37f347deac331e3311d820c3
celia-liao <infinite010750@gmail.com>
2026-06-12 15:07:13 +0800

    更新loading頁面的logo

---
+++ resources/js/components/LoadingOverlay.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="loading-overlay">
+    <div class="images-container">
+      <div class="image-left">
+        <img 
+          :src="assetUrl(`storage/pets/${slug}/image/loading/people/human.svg`)" 
+          alt="飼主" 
+        />
+      </div>
+      <div class="image-right">
+        <img 
+          :src="assetUrl(`storage/pets/${slug}/image/loading/pet/pet.svg`)" 
+          alt="毛孩" 
+        />
+      </div>
+      <div class="line"></div>
+    </div>
+    <div class="slogan">信念科技•全球首創</div>
+    <div class="logo">
+      <img 
+        :src="assetUrl('assets/images/logotype.svg')" 
+        alt="logo" 
+      />
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { useAsset } from '../composables/useAsset';
+
+defineProps<{
+  slug: string;
+}>();
+
+const { assetUrl } = useAsset();
+</script>
